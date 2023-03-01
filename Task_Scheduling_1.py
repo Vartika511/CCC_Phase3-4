6e01966bfc627f9f18103ced019017e95dd64468
SOORAJ TOMAR <91649397+SoorajTomar@users.noreply.github.com>
2023-03-01 13:48:11 +0530

    Create Task_Scheduling_1.py

---
+++ Task_Scheduling_1.py
@@ -0,0 +1,28 @@
+class Job:
+    def __init__(self, taskId, deadline, profit):
+        self.taskId = taskId
+        self.deadline = deadline
+        self.profit = profit
+def scheduleJobs(jobs, T):
+    profit = 0
+    slot = [-1] * T
+    jobs.sort(key=lambda x: x.profit, reverse=True)
+    for job in jobs:
+        for j in reversed(range(job.deadline)):
+            if j < T and slot[j] == -1:
+                slot[j] = job.taskId
+                profit += job.profit
+                break
+    print( profit)
+if __name__ == '__main__':
+    t=int(input())
+    while t!=0:
+        t-=1
+        n=int(input())
+        jobs = []
+        deadline=[int(i) for i in input().split()]
+        profit=[int(i) for i in input().split()]
+        for i in range(n):
+            jobs.append(Job(i+1, deadline[i], profit[i]))
+        maxdead=max(deadline)
+        scheduleJobs(jobs, maxdead)
